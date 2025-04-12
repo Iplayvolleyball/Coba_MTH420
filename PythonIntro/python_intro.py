628ba4c467f560966a8887cd4349e570e796e6a5
Iris <cobai@oregonstate.edu>
2025-04-12 17:51:04 +0000

    almost done

--- PythonIntro/python_intro.py
+++ PythonIntro/python_intro.py
@@ -7,6 +7,9 @@ MTH420
 
 
 # Problem 1 (write code below)
+    
+if __name__ == "__main__":
+    print("Hello, world!")
 
 
 # Problem 2
@@ -14,6 +17,7 @@ def sphere_volume(r):
     """ Return the volume of the sphere of radius 'r'.
     Use 3.14159 for pi in your computation.
     """
+    return (4/3) * (3.14159) * r**3
     raise NotImplementedError("Problem 2 Incomplete")
 
 
@@ -22,8 +26,10 @@ def isolate(a, b, c, d, e):
     """ Print the arguments separated by spaces, but print 5 spaces on either
     side of b.
     """
+    print(a, b, c, sep="     ", end=" ")
+    print(d, e)
+    
     raise NotImplementedError("Problem 3 Incomplete")
-
 
 # Problem 4
 def first_half(my_string):
@@ -36,6 +42,7 @@ def first_half(my_string):
         >>> first_half("ipython")
         'ipy'
     """
+    return my_string[:len(my_string) // 2]
     raise NotImplementedError("Problem 4 Incomplete")
 
 def backward(my_string):
@@ -47,6 +54,7 @@ def backward(my_string):
         >>> backward("ipython")
         'nohtypi'
     """
+    return my_string[::-1]
     raise NotImplementedError("Problem 4 Incomplete")
 
 
@@ -66,6 +74,14 @@ def list_ops():
         >>> list_ops()
         ['fox', 'hawk', 'dog', 'bearhunter']
     """
+    my_list = ["bear", "ant", "cat", "dog"]
+    my_list.append("eagle")
+    my_list[2] = "fox"
+    my_list.remove(my_list[1])
+    my_list.sort(reverse = True)
+    my_list[1] = "hawk"
+    my_list.append("hunter")
+    return my_list
     raise NotImplementedError("Problem 5 Incomplete")
 
 
@@ -79,6 +95,10 @@ def pig_latin(word):
         >>> pig_latin("banana")
         'ananabay'
     """
+    if word[0].lower() in "aeiou":
+        return word + "hay"
+    else:
+        return word[1:] + word[0] + "ay"
     raise NotImplementedError("Problem 6 Incomplete")
 
 
@@ -95,3 +115,29 @@ def alt_harmonic(n):
     harmonic series, which approximates ln(2).
     """
     raise NotImplementedError("Problem 8 Incomplete")
+
+    
+    
+    
+if __name__ == "__main__":
+    print("Hello, world!")
+    
+#Test for Problem 2
+print(sphere_volume(5))
+
+#Test for Problem 3
+print(isolate(1, 2, 3, 4, 5))
+
+#Test for Problem 4
+print(first_half("ciao"))
+print(first_half("bella"))
+print(backward("pizza"))
+               
+#Test for Problem 5
+animals = list_ops()
+print(animals)
+
+#Test for Problem 6
+print(pig_latin("cacca"))
+
+#Test for Problem 7
